b3b3258688c217cafbead2aae40c99dc33405c10
dobbse2 <dobbse2@aalto.fi>
2024-11-26 01:27:34 +0200

    debugs and tests on figures 1-5. Using QubitManager now for some things, still need to swap over entirely

--- cirq_test.py
+++ cirq_test.py
@@ -1,12 +1,24 @@
 import cirq
-from steane_cirq import meas_H_logical_ft_1flag, distill_magic_state, H_logical_nonft
+import matplotlib.pyplot as plt
+from steane_cirq import meas_H_logical_ft_1flag, distill_magic_state, H_logical_nonft, H_logical_ft_flag1, controlled_h
 
 circuit = cirq.Circuit()
+qubitmanager = cirq.SimpleQubitManager()
 
 data_qubits = cirq.LineQubit.range(7)
-ancilla_qubits = [cirq.LineQubit(7), cirq.LineQubit(8)]
+ancilla_qubits = [cirq.LineQubit(7), cirq.LineQubit(8), cirq.LineQubit(9), cirq.LineQubit(10), cirq.LineQubit(11)]
 
-H_logical_nonft(circuit, data_qubits)
-#meas_H_logical_ft_flag1(circuit, data_qubits, ancilla_qubits)
+H_logical_ft_flag1(circuit, data_qubits, ancilla_qubits, qubitmanager)
 
-print(circuit)
+#H_logical_nonft(circuit, data_qubits)
+#meas_H_logical_ft_1flag(circuit, data_qubits, ancilla_qubits, qubitmanager)
+#distill_magic_state(circuit, ancilla_qubits[0], data_qubits[0])
+#circuit.append(cirq.measure(data_qubits[0]))
+
+s = cirq.Simulator()
+result = s.run(circuit, repetitions=1)
+print(result)
+#_ = cirq.plot_state_histogram(result, plt.subplot())
+#plt.show()
+#plt.savefig("test.svg")
+#print(circuit)
--- steane_cirq.py
+++ steane_cirq.py
@@ -1,26 +1,28 @@
 import cirq
 
-''' ancilla is assumed to start in the $\ket{H}$ state
+''' ancilla is assumed to start in the $\ket{0}$ state
 '''
 def distill_magic_state(circuit, ancilla, data, adj=False):
+  circuit.append(cirq.T(ancilla))
   circuit.append(cirq.Y(data).controlled_by(ancilla))
-  
   #measure in the Y-basis
-  circuit.append(cirq.S(ancilla) ** -1) # changing basis
-  circuit.append(cirq.measure(ancilla, key="m")) # measurement with key m
+  #circuit.append(cirq.S(ancilla) ** -1) # changing basis
+  #circuit.append(cirq.H(ancilla))
+  #circuit.append(cirq.measure(ancilla, key="m")) # measurement with key m
+  circuit.append(cirq.measure_single_paulistring(cirq.Y(ancilla) * cirq.I(data)))
 
+  circuit.append(cirq.ResetChannel().on(ancilla))
   #apply correction
-  #circuit.append(cirq.S(data).controlled_by(cirq.KeyCondition("m", 1)))
   if adj:
-    circuit.append((cirq.S(data)**-1).with_classical_controls(cirq.KeyCondition("m", 1))) #probably not 1, but actually 0
+    circuit.append((cirq.S(data)**-1).with_classical_controls(cirq.KeyCondition(str(ancilla)))) #probably not 1, but actually 0
   else:
-    circuit.append(cirq.S(data).with_classical_controls(cirq.KeyCondition("m", 1)))
+    circuit.append(cirq.S(data).with_classical_controls(cirq.KeyCondition(str(ancilla))))
 
 
 
-def controlled_h(circuit, control, target):
+def controlled_h(circuit, control, target, qubitmanager):
   #circuit.append(cirq.T(target) ** -1)
-  magic = cirq.LineQubit(99) # This is terrible practice. I do this as a stopgap solution, but this should be replaced with proper use of QubitManager.qalloc() ASAP (though I believe this requires properly implementing circuit decompositions, etc.)
+  magic = qubitmanager.qalloc(1)[0]
   # apply a T gate with some probability of error to create the magic state
   circuit.append(cirq.T(magic))
   distill_magic_state(circuit, magic, target, adj=True)
@@ -28,28 +30,29 @@ def controlled_h(circuit, control, target):
   circuit.append(cirq.CZ(control, target))
   
   #circuit.append(cirq.T(target))
-  distill_magic_state(circuit, magic, target)
+  magic_2 = qubitmanager.qalloc(1)[0]
+  distill_magic_state(circuit, magic_2, target)
 
-def meas_H_logical_ft_1flag(circuit, data_qubits, ancilla_qubits):
+def meas_H_logical_ft_1flag(circuit, data_qubits, ancilla_qubits, qubitmanager):
   # Use if you want the cirq implemention of CH rather than custom decomp
   # controlled_h = cirq.ControlledGate(sub_gate=cirq.H, num_controls=1)
 
   # send ancilla[0] to $\ket{+}$
   circuit.append(cirq.H(ancilla_qubits[0]))
 
-  controlled_h(circuit, ancilla_qubits[0], data_qubits[6])
+  controlled_h(circuit, ancilla_qubits[0], data_qubits[6], qubitmanager)
   circuit.append(cirq.CNOT(ancilla_qubits[0], ancilla_qubits[1]))
   
-  controlled_h(circuit, ancilla_qubits[0], data_qubits[5])
-  controlled_h(circuit, ancilla_qubits[0], data_qubits[4])
-  controlled_h(circuit, ancilla_qubits[0], data_qubits[3])
-  controlled_h(circuit, ancilla_qubits[0], data_qubits[2])
-  controlled_h(circuit, ancilla_qubits[0], data_qubits[1])
+  controlled_h(circuit, ancilla_qubits[0], data_qubits[5], qubitmanager)
+  controlled_h(circuit, ancilla_qubits[0], data_qubits[4], qubitmanager)
+  controlled_h(circuit, ancilla_qubits[0], data_qubits[3], qubitmanager)
+  controlled_h(circuit, ancilla_qubits[0], data_qubits[2], qubitmanager)
+  controlled_h(circuit, ancilla_qubits[0], data_qubits[1], qubitmanager)
 
   circuit.append(cirq.CNOT(ancilla_qubits[0], ancilla_qubits[1]))
   circuit.append(cirq.measure(ancilla_qubits[1])) # measure Z ancilla[1]
   
-  controlled_h(circuit, ancilla_qubits[0], data_qubits[0])
+  controlled_h(circuit, ancilla_qubits[0], data_qubits[0], qubitmanager)
 
   # measure ancilla[0] in the X-basis
   circuit.append(cirq.H(ancilla_qubits[0]))
@@ -81,7 +84,65 @@ def H_logical_nonft(circuit, data_qubits):
 
   return
 
-def H_logical_ft_flag1(circuit, data_qubits, ancilla_qubits):
+''' Assumes ancilla_qubits is a list of 5 qubits initialized to $\ket{0}$
+'''
+def H_logical_ft_flag1(circuit, data_qubits, ancilla_qubits, qubitmanager):
   H_logical_nonft(circuit, data_qubits)
-  meas_H_logical_ft_1flag(circuit, data_qubits, ancilla_qubits[:2])
-  stab_extract_ft_flag(circuit, data_qubits, ancilla_qubits[2:])
+  meas_H_logical_ft_1flag(circuit, data_qubits, ancilla_qubits[:2], qubitmanager)
+  stab_extract_ft_flag(circuit, data_qubits, ancilla_qubits[2:], qubitmanager)
+  return
+
+def stab_extract_ft_flag(circuit, data_qubits, ancilla_qubits, qubitmanager):
+  circuit.append(cirq.H(ancilla_qubits[0]))
+  circuit.append(cirq.CNOT(ancilla_qubits[0], data_qubits[4]))
+  circuit.append(cirq.CNOT(data_qubits[6], ancilla_qubits[1]))
+  circuit.append(cirq.CNOT(data_qubits[5], ancilla_qubits[2]))
+
+  circuit.append(cirq.CNOT(ancilla_qubits[0], ancilla_qubits[2]))
+
+  circuit.append(cirq.CNOT(ancilla_qubits[0], data_qubits[0]))
+  circuit.append(cirq.CNOT(data_qubits[4], ancilla_qubits[1]))
+  circuit.append(cirq.CNOT(data_qubits[1], ancilla_qubits[2]))
+
+  circuit.append(cirq.CNOT(ancilla_qubits[0], ancilla_qubits[1]))
+
+  circuit.append(cirq.CNOT(ancilla_qubits[0], data_qubits[6]))
+  circuit.append(cirq.CNOT(data_qubits[5], ancilla_qubits[1]))
+  circuit.append(cirq.CNOT(data_qubits[2], ancilla_qubits[2]))
+
+  circuit.append(cirq.H(ancilla_qubits[0]))
+  circuit.append(cirq.measure(ancilla_qubits[0:3]))
+  #circuit.append(cirq.ResetChannel().on_each(ancilla_qubits[0:3]))
+  ancilla_qubits = qubitmanager.qalloc(3)
+  circuit.append(cirq.H(ancilla_qubits[1]))
+  circuit.append(cirq.H(ancilla_qubits[2]))
+
+  circuit.append(cirq.CNOT(data_qubits[4], ancilla_qubits[0]))
+  circuit.append(cirq.CNOT(ancilla_qubits[1], data_qubits[6]))
+  circuit.append(cirq.CNOT(ancilla_qubits[2], data_qubits[5]))
+
+  circuit.append(cirq.CNOT(ancilla_qubits[2], ancilla_qubits[0]))
+
+  circuit.append(cirq.CNOT(data_qubits[0], ancilla_qubits[0]))
+  circuit.append(cirq.CNOT(ancilla_qubits[1], data_qubits[4]))
+  circuit.append(cirq.CNOT(ancilla_qubits[2], data_qubits[1]))
+
+  circuit.append(cirq.CNOT(data_qubits[2], ancilla_qubits[0]))
+  circuit.append(cirq.CNOT(ancilla_qubits[1], data_qubits[3]))
+  circuit.append(cirq.CNOT(ancilla_qubits[2], data_qubits[6]))
+
+  circuit.append(cirq.CNOT(ancilla_qubits[1], ancilla_qubits[0]))
+
+  circuit.append(cirq.CNOT(data_qubits[6], ancilla_qubits[0]))
+  circuit.append(cirq.CNOT(ancilla_qubits[1], data_qubits[5]))
+  circuit.append(cirq.CNOT(ancilla_qubits[2], data_qubits[2]))
+
+  circuit.append(cirq.H(ancilla_qubits[1]))
+  circuit.append(cirq.H(ancilla_qubits[2]))
+  circuit.append(cirq.measure(ancilla_qubits[0:3]))
+
+  return
+
+
+
+
